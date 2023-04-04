657f1f798380a2046a13b059014613e4d33fff1b
Mikael Aurell <mikael.aurell@gmail.com>
2023-04-04 10:48:08 +0200

    end of section 13

--- client-app/src/features/errors/ValidationError.tsx
+++ client-app/src/features/errors/ValidationError.tsx
@@ -1,7 +1,7 @@
 import { Message } from "semantic-ui-react";
 
 interface Props {
-    errors: string[];
+    errors: any;
 }
 
 export default function ValidationError({errors}: Props) {
@@ -9,8 +9,8 @@ export default function ValidationError({errors}: Props) {
         <Message error>
             {errors && (
                 <Message.List>
-                    {errors.map((err: string,  index) => (
-                        <Message.Item key={index}>
+                    {errors.map((err: string,  i: any) => (
+                        <Message.Item key={i}>
                             {err}
                         </Message.Item>
                     ))}
