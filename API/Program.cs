1bd61f2d3e19e4973d14b84679483fbeea2222fe
Mikael Aurell <mikael.aurell@gmail.com>
2023-05-02 11:35:07 +0200

    added headers strict in program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -44,6 +44,14 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+else 
+{
+    app.Use(async (context, next) => 
+    {
+        context.Response.Headers.Add("Strict-Transport-Security", "max-age=31536000");
+        await next.Invoke();
+    });
+}
 
 app.UseCors("CorsPolicy");
 
